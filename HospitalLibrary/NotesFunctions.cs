5b0eec8f1d695d4dee9f24b005b1475bc338c6cb
Eduard <mamaxropela@gmail.com>
2014-10-25 02:28:32 +0300

    add be func for note entity

--- HospitalLibrary/NotesFunctions.cs
+++ HospitalLibrary/NotesFunctions.cs
@@ -14,6 +14,36 @@ namespace HospitalLibrary
 
 		#region Non-async methods
 
+		public Note GetNoteById(int id)
+		{
+			return this.notes.Find(id);
+		}
+
+		public User GetNotesUser(int id)
+		{
+			return GetNoteById(id).user;
+		}
+
+		public Card GetNotesGard(int id)
+		{
+			return GetNoteById(id).card;
+		}
+
+		public bool GetNotesHiddenProperty(int id)
+		{
+			return GetNoteById(id).IsHidden;
+		}
+
+		public DateTime GetNotesDate(int id)
+		{
+			return GetNoteById(id).Date;
+		}
+
+		public string GetNotesText(int id)
+		{
+			return GetNoteById(id).NoteText;
+		}
+
 		#endregion
 	}
 }
