e62dc85d109cc5d7b9e211f5f78cc8964ab6e946
Eduard <mamaxropela@gmail.com>
2014-12-13 15:47:18 +0200

    add card design

--- Hospital/MainWindow.xaml.cs
+++ Hospital/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Hospital.Pages;
 using HospitalLibrary;
 
 namespace Hospital
@@ -24,11 +25,81 @@ namespace Hospital
 	public partial class MainWindow : Window
 	{
 		private HospitalEntities context;
+		private HospitalLibrary.Card _card;
+		public HospitalLibrary.Card Card
+		{
+			get { return _card; }
+			private set
+			{
+				_card = value;
+				UserName = value.Name;
+				UserAge = value.PatientAge;
+				UserAgain = value.IsAgain ? "Again" : "New";
+				UserNote = value.NoteText;
+			}
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			context = new HospitalEntities();
-			
+			context.cards.Load();
+			var cards = context.cards.ToList();
+			foreach (var card in cards)
+			{
+				var UIcard = new Hospital.Pages.Card(card);
+				UIcard.MouseLeftButtonDown += (sender, args) => 
+					Card = ((Hospital.Pages.Card) sender).UserCard;
+				CardContainer.Children.Add(UIcard);
+			}
 		}
+
+		#region Dependendency prop
+
+		public static readonly DependencyProperty UserNameProperty = DependencyProperty.Register(
+			"UserName", typeof (String), typeof (MainWindow), new PropertyMetadata(default(String)));
+
+		public String UserName
+		{
+			get { return (String) GetValue(UserNameProperty); }
+			set { SetValue(UserNameProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserAgeProperty = DependencyProperty.Register(
+			"UserAge", typeof (int), typeof (MainWindow), new PropertyMetadata(default(int)));
+
+		public int UserAge
+		{
+			get { return (int) GetValue(UserAgeProperty); }
+			set { SetValue(UserAgeProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserSexProperty = DependencyProperty.Register(
+			"UserSex", typeof (string), typeof (MainWindow), new PropertyMetadata(default(string)));
+
+		public string UserSex
+		{
+			get { return (string) GetValue(UserSexProperty); }
+			set { SetValue(UserSexProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserAgainProperty = DependencyProperty.Register(
+			"UserAgain", typeof (string), typeof (MainWindow), new PropertyMetadata(default(string)));
+
+		public string UserAgain
+		{
+			get { return (string) GetValue(UserAgainProperty); }
+			set { SetValue(UserAgainProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserNoteProperty = DependencyProperty.Register(
+			"UserNote", typeof (string), typeof (MainWindow), new PropertyMetadata(default(string)));
+
+		public string UserNote
+		{
+			get { return (string) GetValue(UserNoteProperty); }
+			set { SetValue(UserNoteProperty, value); }
+		}
+		#endregion
 	}
 }
---
+++ Hospital/Pages/Card.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HospitalLibrary;
+
+namespace Hospital.Pages
+{
+	/// <summary>
+	/// Interaction logic for Card.xaml
+	/// </summary>
+	public partial class Card : UserControl
+	{
+		public HospitalLibrary.Card UserCard { get; private set; }
+		public Card(HospitalLibrary.Card userCard)
+		{
+			InitializeComponent();
+			UserCard = userCard;
+			UserName = UserCard.Name;
+			UserAge = UserCard.PatientAge;
+			UserSex = UserCard.PatientSex.ToLower() == "male" ? "Man" : "Woman";
+		}
+
+		public static readonly DependencyProperty UserNameProperty = DependencyProperty.Register(
+			"UserName", typeof (String), typeof (Card), new PropertyMetadata(default(String)));
+
+		public String UserName
+		{
+			get { return (String) GetValue(UserNameProperty); }
+			set { SetValue(UserNameProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserAgeProperty = DependencyProperty.Register(
+			"UserAge", typeof (int), typeof (Card), new PropertyMetadata(default(int)));
+
+		public int UserAge
+		{
+			get { return (int) GetValue(UserAgeProperty); }
+			set { SetValue(UserAgeProperty, value); }
+		}
+
+		public static readonly DependencyProperty UserSexProperty = DependencyProperty.Register(
+			"UserSex", typeof (String), typeof (Card), new PropertyMetadata(default(String)));
+
+		public String UserSex
+		{
+			get { return (String) GetValue(UserSexProperty); }
+			set { SetValue(UserSexProperty, value); }
+		}
+	}
+}
